715a27344fe9795155c00a23dbdb7acffac4efe5
catbouce <75095386+catbouce@users.noreply.github.com>
2023-05-27 20:18:16 -0700

    feat: add styles to Login page

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,8 +1,45 @@
 import styled from 'styled-components';
 
 export const AppContainer = styled.div`
-  display: flex;
   height: 100vh;
+  display: flex;
   flex: 1;
   flex-direction: column;
 `;
+
+export const primaryColor = '#4d4dff';
+export const primaryColorHover = '#6666ff';
+
+export const materialDefaultTheme: { [key: string]: any } = {
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline,.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline, .MuiInputLabel-root.Mui-focused ':
+            {
+              borderColor: primaryColor,
+              color: primaryColor,
+            },
+        },
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: primaryColor,
+          textDecoration: 'none',
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: primaryColor,
+          '&:hover': {
+            backgroundColor: primaryColorHover,
+          },
+        },
+      },
+    },
+  },
+};
